2b3f473acfac9f56a4bebdcbc09abe3a9fc18a5f
BOoll18 <35131621+BOoll18@users.noreply.github.com>
2018-12-30 15:36:36 +0200

    Add files via upload

---
+++ Първа задача от глава 3/src/pkg1st/exercise/Exercise.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1st.exercise;
+
+/**
+ *
+ * @author BOoll18
+ */
+public class Exercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number=2;
+        if(number%2==0)
+        {
+            System.out.print("Countable");
+        }
+        else
+        {
+            System.out.print("Uncountable");
+        }
+    }
+    
+}
